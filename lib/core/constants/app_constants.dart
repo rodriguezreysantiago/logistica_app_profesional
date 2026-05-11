170ea1db506ab750f4bce0882c35e487405beeef
rodriguezreysantiago <santiagorodriguezrey@gmail.com>
2026-05-10 21:12:48 -0300

    chore: bump version 1.0.42+45

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -82,7 +82,7 @@ class AppTexts {
 
   static const String rutaNoEncontrada = 'Ruta no encontrada';
   // Podés mantener un registro visual de tu versión acá
-  static const String appVersion = 'v 1.0.41';
+  static const String appVersion = 'v 1.0.42';
 }
 
 // ===========================================================================
